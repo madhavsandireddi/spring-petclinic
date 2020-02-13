a458a7ca7408014e4b278677899adee8af096e2a
madhav <madhav.5254@gmail.com>
2020-02-13 12:18:49 +0530

    added file

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+#pollscm
 package org.springframework.samples.petclinic;
 
 import org.springframework.boot.SpringApplication;
